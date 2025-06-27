ecdf8aea1e0bea3510b3b5a12866ca6474cc62f3
Radilovk <radilov.k@gmail.com>
2025-06-27 21:33:54 +0300

    chore: remove unused FROM_EMAIL_VAR_NAME

--- preworker.js
+++ preworker.js
@@ -29,7 +29,6 @@ import { sendEmail as phpSendEmail } from './sendEmailWorker.js';
 /** @type {(to: string, subject: string, body: string) => Promise<void>} */
 let sendEmailFn = defaultSendEmail;
 const MAILER_ENDPOINT_URL_VAR_NAME = 'MAILER_ENDPOINT_URL';
-const FROM_EMAIL_VAR_NAME = 'FROM_EMAIL';
 async function getSendEmail(env) {
     if (sendEmailFn && sendEmailFn !== defaultSendEmail) return sendEmailFn;
     const endpoint = env?.[MAILER_ENDPOINT_URL_VAR_NAME];
