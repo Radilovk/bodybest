07065f88fc4ed03f3e8d8f24b71f097ad83cde6c
Radilovk <radilov.k@gmail.com>
2025-07-23 02:48:10 +0300

    Replace progress-mask with progress-fill

--- js/__tests__/populateUI.test.js
+++ js/__tests__/populateUI.test.js
@@ -9,9 +9,9 @@ beforeEach(async () => {
     <h1 id="headerTitle"></h1>
     <div id="goalCard"></div><div id="engagementCard"></div><div id="healthCard"></div>
     <div id="progressHistoryCard"></div>
-    <div id="goalProgressMask"></div><div id="goalProgressBar"></div><span id="goalProgressText"></span>
-    <div id="engagementProgressMask"></div><div id="engagementProgressBar"></div><span id="engagementProgressText"></span>
-    <div id="healthProgressMask"></div><div id="healthProgressBar"></div><span id="healthProgressText"></span>
+    <div id="goalProgressFill"></div><div id="goalProgressBar"></div><span id="goalProgressText"></span>
+    <div id="engagementProgressFill"></div><div id="engagementProgressBar"></div><span id="engagementProgressText"></span>
+    <div id="healthProgressFill"></div><div id="healthProgressBar"></div><span id="healthProgressText"></span>
     <div id="streakGrid"></div><span id="streakCount"></span>
     <h3 id="dailyPlanTitle"></h3>
     <ul id="dailyMealList"></ul>
@@ -23,13 +23,13 @@ beforeEach(async () => {
     engagementCard: document.getElementById('engagementCard'),
     healthCard: document.getElementById('healthCard'),
     progressHistoryCard: document.getElementById('progressHistoryCard'),
-    goalProgressMask: document.getElementById('goalProgressMask'),
+    goalProgressFill: document.getElementById('goalProgressFill'),
     goalProgressBar: document.getElementById('goalProgressBar'),
     goalProgressText: document.getElementById('goalProgressText'),
-    engagementProgressMask: document.getElementById('engagementProgressMask'),
+    engagementProgressFill: document.getElementById('engagementProgressFill'),
     engagementProgressBar: document.getElementById('engagementProgressBar'),
     engagementProgressText: document.getElementById('engagementProgressText'),
-    healthProgressMask: document.getElementById('healthProgressMask'),
+    healthProgressFill: document.getElementById('healthProgressFill'),
     healthProgressBar: document.getElementById('healthProgressBar'),
     healthProgressText: document.getElementById('healthProgressText'),
     streakGrid: document.getElementById('streakGrid'),
