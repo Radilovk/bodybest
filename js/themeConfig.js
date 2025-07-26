c58ac0592106ce90d1c70393a11ae9c633559b74
Radilovk <radilov.k@gmail.com>
2025-07-26 18:15:35 +0300

    expand theme options and use css vars

--- js/themeConfig.js
+++ js/themeConfig.js
@@ -25,5 +25,23 @@ export const colorGroups = [
       { var: 'card-bg', label: 'Фон на карти' },
       { var: 'input-bg', label: 'Фон на полета' }
     ]
+  },
+  {
+    name: 'Граници и полета',
+    items: [
+      { var: 'text-color-on-secondary', label: 'Текст върху втори цвят' },
+      { var: 'text-color-disabled', label: 'Неактивен текст' },
+      { var: 'border-color', label: 'Цвят на рамките' },
+      { var: 'border-color-soft', label: 'Слаб цвят на рамки' },
+      { var: 'input-bg-disabled', label: 'Фон на деактивирано поле' },
+      { var: 'input-border-color', label: 'Рамка на полета' }
+    ]
+  },
+  {
+    name: 'Прогрес барове',
+    items: [
+      { var: 'progress-end-color', label: 'Краен цвят на прогрес' },
+      { var: 'progress-bar-bg-empty', label: 'Празен прогрес бар' }
+    ]
   }
 ];
