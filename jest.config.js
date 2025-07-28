175a94fcc87ba9e711682d59560db4e8a893cd19
Radilovk <radilov.k@gmail.com>
2025-07-29 02:57:45 +0300

    docs: add troubleshooting note for repo download failures

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   // Use jsdom for DOM APIs in tests
   testEnvironment: 'jsdom',
-  // Treat TypeScript files as ES modules (future-proof)
+  // Treat TypeScript files as ES modules (JavaScript is inferred from package.json)
   extensionsToTreatAsEsm: ['.ts'],
   // Disable Babel; rely on native ESM support
   transform: {},
