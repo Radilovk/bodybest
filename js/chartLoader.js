78c39f264ad6156c46802f38d67a2be5b7a19f4b
Radilovk <radilov.k@gmail.com>
2025-08-02 04:05:26 +0300

    feat: pin chart.js import

--- js/chartLoader.js
+++ js/chartLoader.js
@@ -5,7 +5,9 @@ export async function ensureChart() {
         || (typeof process !== 'undefined' && process.env?.NODE_ENV === 'test')) {
       ChartLib = () => ({ destroy() {} });
     } else {
-      ChartLib = (await import('https://cdn.jsdelivr.net/npm/chart.js')).default;
+      const mod = await import('https://cdn.jsdelivr.net/npm/chart.js@4/dist/chart.umd.js');
+      ChartLib = mod.Chart || mod.default;
+      if (typeof ChartLib !== 'function') throw new Error('Chart.js not loaded');
       console.debug('Chart.js loaded');
     }
   }
